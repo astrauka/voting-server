f4b23d90f6840ceafcf62c8c496c77a6f37d8132
Karolis Astrauka <astrauka@gmail.com>
2015-09-22 14:22:41 +0300

    Add redux store

---
+++ test/store_spec.js
@@ -0,0 +1,21 @@
+import {Map, List, fromJS} from 'immutable';
+import {expect} from 'chai';
+
+import makeStore from '../src/store';
+
+describe('makeStore', () => {
+  it('is a redux store', () => {
+    const store = makeStore();
+    const entries = List.of('Trainspotting', '28');
+    expect(store.getState()).to.equal(Map());
+
+    store.dispatch({
+      type: 'SET_ENTRIES',
+      entries: entries
+    });
+
+    expect(store.getState()).to.equal(fromJS({
+      entries: entries
+    }));
+  });
+});
